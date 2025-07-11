fa1cfbc4d8fbfcbdaf3cf165a627ba28c2539188
Pulkit007731 <pulkitd82@gmail.com>
2025-07-11 11:36:44 +0530

    Added GitHub Actions deploy.yml

---
+++ Hello_world_devops_testing/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello_world_devops_testing.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World – CI/CD test successful!");
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# hello-world-devops-api
-A simple ASP.NET Core Web API project with CI/CD
